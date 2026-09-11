b23755c85323c1c5ad9719f2d49b63fbdad85ab0
gilsongillazaro-cyber <gilsongillazaro.com>
2026-09-11 01:06:44 +0100

    adicao do analitics da vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import UserSContextProvider from "../Context/UserSelecionadoContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <>
     <UserSContextProvider>
       <ToastContainer />
+      <Analytics />
       <App />
     </UserSContextProvider>
   </>,
